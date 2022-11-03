49f52dc479f6e1d7bc4d0021dbbdd993c7d5bcda
= <leia.glezer12@gmail.com>
2022-11-03 12:10:41 -0400

    chord/note/volume working in game loop

--- game.py
+++ game.py
@@ -7,7 +7,6 @@ class Game:
     def __init__(self):
         ######## APPLICATION SETUP ATTRIBUTES ##########
         #turn game on
-        self.curr_char = None
         pygame.init()
         # background image
         self.background = pygame.image.load('splash.png')
@@ -36,6 +35,9 @@ class Game:
         self.curr_volume = None
         self.curr_chord = None
         self.timbre = "timbre1"
+        self.curr_note = None
+        self.x = None
+        self.y = None
 
         ######## MUSIC SETUP ##########
         # setup music player
@@ -65,6 +67,8 @@ class Game:
         ######## INITIAL CHARACTER SETUP ##########
         # instantiate three chars
         self.char_list = [Player(), Player(), Player()]
+        self.curr_char = None
+
         # set initial image/location
         for char in self.char_list:
             char.set_frame()
@@ -79,7 +83,7 @@ class Game:
                 self.display.blit(pygame.image.load('main.png'), (0, 0))
 
                 #get fresh mouse coordinates
-                x, y = pygame.mouse.get_pos()
+                self.x, self.y = pygame.mouse.get_pos()
 
                 # checks if player wants to quit game
                 if event.type == pygame.QUIT:
@@ -94,7 +98,7 @@ class Game:
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     # checks if user selects individual char or entire choir
                     for char in self.char_list:
-                        char.set_selected_blob(x, y)
+                        char.set_selected_blob(self.x, self.y)
                         if char.IS_SELECTED:
                             self.curr_status = 'Solo Mode'
                             self.curr_char = char
@@ -109,12 +113,9 @@ class Game:
                 if event.type == pygame.MOUSEMOTION:
                     for i, char in enumerate(self.char_list):
                         #frame stuff
-                        char.set_note_and_chord(x, i)
-                        char.set_volume(y)
+                        char.set_note_and_chord(self.x, i)
+                        char.set_volume(self.y)
                         char.set_frame()
-
-
-
 
                 # 4. ####### ANIMATION #######
                 # draws chars to display
@@ -127,44 +128,81 @@ class Game:
             self.reset_key()
 
             # 6. ####### MUSIC PLAYING #######
-            if self.curr_status == 'Multi Mode':
-                # If the mouse motion is within the x bound of the current chord, don't restart the audio, just keep playing.
-                if self.curr_chord == self.char_list[0].get_chord():
-                    self.curr_volume = self.char_list[0].get_volume()
-                    self.set_volume()
-                    continue
-
-                # the mouse has moved to the x bound of a new chord, play new chord.
+            ###chord stuff
+            # update chord
+            if self.x is not None:
+                if self.curr_status == 'Multi Mode':
+                    # update chord based on x position
+                    if self.x < 100:
+                        self.play_chord("Cmaj")
+                        self.curr_chord = "Cmaj"
+                    elif (self.x > 100) & (self.x < 200):
+                        self.play_chord("Dmin")
+                        self.curr_chord = "Dmin"
+                    elif (self.x > 200) & (self.x < 300):
+                        self.play_chord("Emin")
+                        self.curr_chord = "Emin"
+                    elif (self.x > 300) & (self.x < 400):
+                        self.play_chord("Fmaj")
+                        self.curr_chord = "Fmaj"
+                    elif (self.x > 400) & (self.x < 500):
+                        self.play_chord("Gmaj")
+                        self.curr_chord = "Gmaj"
+                    elif (self.x > 500) & (self.x < 600):
+                        self.play_chord("Amin")
+                        self.curr_chord = "Amin"
+                    elif (self.x > 600) & (self.x < 700):
+                        self.play_chord("Bdim")
+                        self.curr_chord = "Bdim"
+                #single mode
                 else:
-                    self.play_chord(self.curr_chord)
-                    self.curr_volume = self.char_list[0].get_volume()
-                    self.curr_chord = self.char_list[0].get_chord()
-                    self.set_volume()
+                    # update chord based on x position
+                    if self.x < 100:
+                        self.play_note(0)
+                        self.curr_note = 0
+                    elif (self.x > 100) & (self.x < 200):
+                        self.play_note(1)
+                        self.curr_note = 1
+                    elif (self.x > 200) & (self.x < 300):
+                        self.play_note(2)
+                        self.curr_note = 2
+                    elif (self.x > 300) & (self.x < 400):
+                        self.play_note(3)
+                        self.curr_note = 3
+                    elif (self.x > 400) & (self.x < 500):
+                        self.play_note(4)
+                        self.curr_note = 4
+                    elif (self.x > 500) & (self.x < 600):
+                        self.play_note(5)
+                        self.curr_note = 5
+                    elif (self.x > 600) & (self.x < 700):
+                        self.play_note(6)
+                        self.curr_note = 6
 
-            # same logic as above, but for notes in "Single Mode"
-            else:
-                if self.curr_pitch == self.curr_char.get_pitch():
-                    self.set_volume()
-                    continue
+            #update volume
+            if self.y is not None:
+                # volume update
+                if self.y > 350:
+                    self.curr_volume = 0
+                elif (self.y < 350) & (self.y > 200):
+                    self.curr_volume = 0.5
+                elif self.y < 200:
+                    self.curr_volume = 1.0
                 else:
-                    self.play_note(self.curr_char.get_pitch())
-                    self.curr_pitch = self.curr_char.get_pitch()
-                    self.set_volume()
+                    self.curr_volume = 0.0
 
-
-    def set_volume(self):
-        if self.curr_status == 'Multi Mode':
-            for channel in range(0, 50):
-                pygame.mixer.Channel(channel).set_volume(self.curr_volume/2.0)
-        else:
-            pygame.mixer.Channel(1).set_volume(self.curr_volume/2.0)
+                for channel in range(0, 50):
+                    pygame.mixer.Channel(channel).set_volume(self.curr_volume)
 
     def play_chord(self, chord):
-        if chord is not None:
-            for i, note in enumerate(self.chords[chord]):
-                self.play_note(note, i)
+        if chord == self.curr_chord or chord is None:
+            return
+        for i, note in enumerate(self.chords[chord]):
+            self.play_note(note, i)
 
     def play_note(self, note, channel=1):
+        if note == self.curr_note or note is None:
+            return
         file = self.notes[note][self.timbre]
         print(file)
         sound = pygame.mixer.Sound(file)
@@ -182,14 +220,11 @@ class Game:
     def reset_canvas(self):
         # redraw fresh canvas
         self.display.fill(self.WHITE)
-
     def reset_key(self):
         self.start = False
-
     def quit_game(self):
         self.running = False
         self.playing = False
-
     def change_screens(self):
         self.start = True
         self.playing = False
