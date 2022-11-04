aafddf6478551d80cd8339a45b6cc16a175ee6af
= <leia.glezer12@gmail.com>
2022-11-04 11:21:26 -0400

    Music working, need to work on start/stop music, and add wav files.

--- game.py
+++ game.py
@@ -38,6 +38,7 @@ class Game:
         self.curr_note = None
         self.x = None
         self.y = None
+        self.start_music = False
 
         ######## MUSIC SETUP ##########
         # setup music player
@@ -68,16 +69,25 @@ class Game:
         # instantiate three chars
         self.char_list = [Player(), Player(), Player()]
         self.curr_char = None
+        self.curr_frame_volume = None
 
         # set initial image/location
-        for char in self.char_list:
-            char.set_frame()
-            char.set_location(self.char_list.index(char))
+        for i, char in enumerate(self.char_list):
+            char.frame = char.frame_list[i]
+            char.set_location(i)
+
+        ######## SPRITESHEET REFERENCES ##########
+        # get x,y of frame bounding box
+        self.x_frame = self.char_list[0].frame_list[0].get_rect().x
+        self.y_frame = self.char_list[0].frame_list[0].get_rect().y
+
+        # width/height each frame (all the same size, so can grab any image)
+        self.width = self.char_list[0].frame_list[0].get_width()
+        self.height = self.char_list[0].frame_list[0].get_height()
     def game_loop(self):
         # only plays when player is IN game
         while self.playing:
             for event in pygame.event.get():
-
                 # 1. ####### GAME SETUP #######
                 # reset background so you get a white screen
                 self.display.blit(pygame.image.load('main.png'), (0, 0))
@@ -96,119 +106,154 @@ class Game:
 
                 # 2. ####### NEW MOUSECLICK: PLAY MODE SELECTION #######
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    # checks if user selects individual char or entire choir
-                    for char in self.char_list:
-                        char.set_selected_blob(self.x, self.y)
-                        if char.IS_SELECTED:
-                            self.curr_status = 'Solo Mode'
-                            self.curr_char = char
-                        if char.ALL_SELECTED:
-                            self.curr_status = 'Multi Mode'
+                    #changes between single and multi mode
+                    self.update_mode()
 
                     # Stop current music, so that correct music starts playing for single/multimode.
                     for channel in range(0, 50):
                         pygame.mixer.Channel(channel).stop()
 
-                # 3. ####### NEW MOUSEMOTION: CHORD OR NOTE SELECTION #######
+                # 3. ####### CHORD OR NOTE SELECTION #######
                 if event.type == pygame.MOUSEMOTION:
-                    for i, char in enumerate(self.char_list):
-                        #frame stuff
-                        char.set_note_and_chord(self.x, i)
-                        char.set_volume(self.y)
-                        char.set_frame()
+                    self.update_sprite_frame()
 
                 # 4. ####### ANIMATION #######
-                # draws chars to display
-                for char in self.char_list:
-                    char.draw(self.display)
-                self.window.blit(self.display, (0, 0))
-                pygame.display.update()
+                self.draw_sprite()
 
-            # 5. ####### RESET START KEY #######
-            self.reset_key()
+                # 5. ####### MUSIC PLAYING #######
+                self.update_chord_or_note()
+                self.update_volume()
 
-            # 6. ####### MUSIC PLAYING #######
-            ###chord stuff
-            # update chord
-            if self.x is not None:
-                if self.curr_status == 'Multi Mode':
-                    # update chord based on x position
-                    if self.x < 100:
-                        self.play_chord("Cmaj")
-                        self.curr_chord = "Cmaj"
-                    elif (self.x > 100) & (self.x < 200):
-                        self.play_chord("Dmin")
-                        self.curr_chord = "Dmin"
-                    elif (self.x > 200) & (self.x < 300):
-                        self.play_chord("Emin")
-                        self.curr_chord = "Emin"
-                    elif (self.x > 300) & (self.x < 400):
-                        self.play_chord("Fmaj")
-                        self.curr_chord = "Fmaj"
-                    elif (self.x > 400) & (self.x < 500):
-                        self.play_chord("Gmaj")
-                        self.curr_chord = "Gmaj"
-                    elif (self.x > 500) & (self.x < 600):
-                        self.play_chord("Amin")
-                        self.curr_chord = "Amin"
-                    elif (self.x > 600) & (self.x < 700):
-                        self.play_chord("Bdim")
-                        self.curr_chord = "Bdim"
-                #single mode
-                else:
-                    # update chord based on x position
-                    if self.x < 100:
-                        self.play_note(0)
-                        self.curr_note = 0
-                    elif (self.x > 100) & (self.x < 200):
-                        self.play_note(1)
-                        self.curr_note = 1
-                    elif (self.x > 200) & (self.x < 300):
-                        self.play_note(2)
-                        self.curr_note = 2
-                    elif (self.x > 300) & (self.x < 400):
-                        self.play_note(3)
-                        self.curr_note = 3
-                    elif (self.x > 400) & (self.x < 500):
-                        self.play_note(4)
-                        self.curr_note = 4
-                    elif (self.x > 500) & (self.x < 600):
-                        self.play_note(5)
-                        self.curr_note = 5
-                    elif (self.x > 600) & (self.x < 700):
-                        self.play_note(6)
-                        self.curr_note = 6
+                # 6. ####### RESET START KEY #######
+                self.reset_key()
 
-            #update volume
-            if self.y is not None:
-                # volume update
-                if self.y > 350:
-                    self.curr_volume = 0
-                elif (self.y < 350) & (self.y > 200):
-                    self.curr_volume = 0.5
-                elif self.y < 200:
-                    self.curr_volume = 1.0
-                else:
-                    self.curr_volume = 0.0
 
-                for channel in range(0, 50):
-                    pygame.mixer.Channel(channel).set_volume(self.curr_volume)
+    def update_mode(self):
+        # checks if user selects individual char or entire choir
+        for char in self.char_list:
+            char.set_selected_blob(self.x, self.y)
+            if char.IS_SELECTED:
+                self.curr_status = 'Single Mode'
+                self.curr_char = char
+            if char.ALL_SELECTED:
+                self.curr_status = 'Multi Mode'
 
     def play_chord(self, chord):
+        #plays all notes in chord list
         if chord == self.curr_chord or chord is None:
             return
         for i, note in enumerate(self.chords[chord]):
             self.play_note(note, i)
 
     def play_note(self, note, channel=1):
-        if note == self.curr_note or note is None:
+        #plays all notes in note list, continues playing if still within x boundary.
+        if note == self.curr_note:
             return
         file = self.notes[note][self.timbre]
         print(file)
         sound = pygame.mixer.Sound(file)
+        #pygame.mixer.music.fadeout(10)
         pygame.mixer.Channel(channel).play(sound)
 
+    def update_sprite_frame(self):
+        #updates sprite's current frame based on multi or single mode
+        if self.curr_status == 'Multi Mode':
+            for i, char in enumerate(self.char_list):
+                frame = 3 * self.chords[self.curr_chord][i] + self.curr_frame_volume
+                char.frame = char.frame_list[frame]
+        else:
+            frame = 3 * self.curr_note + self.curr_frame_volume
+            self.curr_char.frame = self.curr_char.frame_list[frame]
+
+    def draw_sprite(self):
+        #draw all chars in char list
+        if self.curr_status == 'Multi Mode':
+            for char in self.char_list:
+                char.draw(self.display, char.frame)
+        # same logic but for individual char
+        # displays "closed mouth" for chars that aren't currently playing
+        else:
+            for char in self.char_list:
+                if char == self.curr_char:
+                    self.curr_char.draw(self.display, self.curr_char.frame)
+                else:
+                    char.draw(self.display, char.frame_list[0])
+
+        self.window.blit(self.display, (0, 0))
+        pygame.display.update()
+
+    def update_chord_or_note(self):
+        if self.x is not None:
+            if self.curr_status == 'Multi Mode':
+                # update chord based on x position
+                if (self.x > 0) & (self.x < 100):
+                    self.play_chord("Cmaj")
+                    self.curr_chord = "Cmaj"
+                elif (self.x > 100) & (self.x < 200):
+                    self.play_chord("Dmin")
+                    self.curr_chord = "Dmin"
+                elif (self.x > 200) & (self.x < 300):
+                    self.play_chord("Emin")
+                    self.curr_chord = "Emin"
+                elif (self.x > 300) & (self.x < 400):
+                    self.play_chord("Fmaj")
+                    self.curr_chord = "Fmaj"
+                elif (self.x > 400) & (self.x < 500):
+                    self.play_chord("Gmaj")
+                    self.curr_chord = "Gmaj"
+                elif (self.x > 500) & (self.x < 600):
+                    self.play_chord("Amin")
+                    self.curr_chord = "Amin"
+                elif (self.x > 600) & (self.x < 700):
+                    self.play_chord("Bdim")
+                    self.curr_chord = "Bdim"
+
+            # single mode
+            else:
+                # update note based on x position
+                if self.x < 100:
+                    self.play_note(0)
+                    self.curr_note = 0
+                elif (self.x > 100) & (self.x < 200):
+                    self.play_note(1)
+                    self.curr_note = 1
+                elif (self.x > 200) & (self.x < 300):
+                    self.play_note(2)
+                    self.curr_note = 2
+                elif (self.x > 300) & (self.x < 400):
+                    self.play_note(3)
+                    self.curr_note = 3
+                elif (self.x > 400) & (self.x < 500):
+                    self.play_note(4)
+                    self.curr_note = 4
+                elif (self.x > 500) & (self.x < 600):
+                    self.play_note(5)
+                    self.curr_note = 5
+                elif (self.x > 600) & (self.x < 700):
+                    self.play_note(6)
+                    self.curr_note = 6
+                else:
+                    return
+    def update_volume(self):
+        if self.y is not None:
+            # volume update based on y position
+            if self.y > 350:
+                self.curr_volume = 0
+                self.curr_frame_volume = 0
+            elif (self.y < 350) & (self.y > 200):
+                self.curr_volume = 0.5
+                self.curr_frame_volume = 1
+            elif self.y < 200:
+                self.curr_volume = 1.0
+                self.curr_frame_volume = 2
+            else:
+                self.curr_volume = 0.0
+                self.curr_frame_volume = 0
+
+            for channel in range(0, 50):
+                pygame.mixer.Channel(channel).set_volume(self.curr_volume)
     def check_events(self):
+        #checks for which menu to display
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
--- player.py
+++ player.py
@@ -13,35 +13,23 @@ from spritesheet import Spritesheet
 # A minor: A – C – E
 # B diminished: B – D – F
 
-chord_list = [[2, 4, 3], [3, 5, 0], [4, 6, 1], [5, 0, 2], [6, 1, 3], [0, 2, 4], [1, 3, 5]]
-pitch_list = [0, 1, 2, 3, 4, 5, 6]
-
 class Player:
     def __init__(self):
-
         # load frames into array
         self.frame_list = self.load_frames()
 
         # get x,y of frame bounding box
         self.x = self.frame_list[0].get_rect().x
         self.y = self.frame_list[0].get_rect().y
-
         # width/height each frame (all the same size, so can grab any image)
         self.width = self.frame_list[0].get_width()
         self.height = self.frame_list[0].get_height()
-
-        # sets initial
-        self.volume = 0
-        self.note = 0
-        self.chord = None
 
         # flag for one char selected
         self.IS_SELECTED = False
         # flag for all chars selected
         self.ALL_SELECTED = True
 
-        # animation frame
-        self.frame = self.frame_list[0]
 
     # determines if one or all blobs are selected
     def set_selected_blob(self, mouseX, mouseY):
@@ -49,92 +37,21 @@ class Player:
         if (mouseX > self.x) & (mouseX < self.x + self.width) & (mouseY > self.y) & (mouseY < self.y + self.height):
             self.IS_SELECTED = True
             self.ALL_SELECTED = False
-
         # all selected, picked random x value it has to be greater then, will be button later
         elif mouseX > 700:
             self.ALL_SELECTED = True
             self.IS_SELECTED = False
-
         else:
             self.IS_SELECTED = False
             self.ALL_SELECTED = False
             return None
 
-    def set_note_and_chord(self, x, i):
-        # if one char selected
-        if self.ALL_SELECTED:
-            # update chord based on x position
-            if x < 100:
-                self.note = chord_list[0][i]
-                self.chord = "Cmaj"
-            elif (x > 100) & (x < 200):
-                self.note = chord_list[1][i]
-                self.chord = "Dmin"
-            elif (x > 200) & (x < 300):
-                self.note = chord_list[2][i]
-                self.chord = "Emin"
-            elif (x > 300) & (x < 400):
-                self.note = chord_list[3][i]
-                self.chord = "Fmaj"
-            elif (x > 400) & (x < 500):
-                self.note = chord_list[4][i]
-                self.chord = "Gmaj"
-            elif (x > 500) & (x < 600):
-                self.note = chord_list[5][i]
-                self.chord = "Amin"
-            elif (x > 600) & (x < 700):
-                self.note = chord_list[6][i]
-                self.chord = "Bdim"
-
-        # if one char is selected
-        elif self.IS_SELECTED:
-            # pitch update
-            if x < 100:
-                self.note = pitch_list[0]
-            elif (x > 100) & (x < 200):
-                self.note = pitch_list[1]
-            elif (x > 200) & (x < 300):
-                self.note = pitch_list[2]
-            elif (x > 300) & (x < 400):
-                self.note = pitch_list[3]
-            elif (x > 400) & (x < 500):
-                self.note = pitch_list[4]
-            elif (x > 500) & (x < 600):
-                self.note = pitch_list[5]
-            elif x > 600:
-                self.note = pitch_list[6]
-    def set_volume(self, y):
-        if self.ALL_SELECTED or self.IS_SELECTED:
-            # volume update
-            if y > 350:
-                self.volume = 0
-            elif (y < 350) & (y > 200):
-                self.volume = 1
-            elif y < 200:
-                self.volume = 2
-        else:
-            self.volume = 0
-    def set_frame(self):
-        # keeps closed mouth animation
-        if self.volume == 0:
-            self.frame = self.frame_list[3 * self.note]
-        else:
-            self.frame = self.frame_list[3 * self.note + self.volume]
-    def set_location(self, frame):
-        self.x = frame * 150 + 165
+    def set_location(self, index):
+        self.x = index * 150 + 165
         self.y = 150
 
-    def get_pitch(self):
-        return self.note
-
-    def get_chord(self):
-        return self.chord
-
-    def get_volume(self):
-        return self.volume
-
-    def draw(self, display):
-        display.blit(self.frame, (self.x, self.y))
+    def draw(self, display, frame):
+        display.blit(frame, (self.x, self.y))
 
     def load_frames(self):
         my_spritesheet = Spritesheet('image.png')
