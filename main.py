c863750bd31f4f45cd0179171082d825f5f92138
Elijah Hopper <elu@lawn-128-61-29-152.lawn.gatech.edu>
2022-11-08 16:41:58 -0500

    run without gloves

--- main.py
+++ main.py
@@ -2,17 +2,14 @@ import sys
 import pygame
 import threading
 from game import Game
-from ApplicationBLE import RightHand
-from ApplicationBLE_left import LeftHand
 from DummyBLE import DummyRightHand
 
 if __name__ == "__main__":
     # everything in init in game will run when this runs.
     # creates game object
 
-    rh = RightHand()
-    lh = LeftHand()
-    # rh = DummyRightHand()
+    rh = DummyRightHand()
+    lh = DummyRightHand()
 
     gloves = (lh, rh)
     g = Game(gloves)
@@ -23,10 +20,6 @@ if __name__ == "__main__":
 
     l_ble_handler = threading.Thread(target=lh.run)
     l_ble_handler.start()
-
-    # while rh.getData('isConnected') == 0:
-    #     # Glove Connection Wait
-    #     continue
 
     while g.running:
         g.curr_menu.display_menu()
